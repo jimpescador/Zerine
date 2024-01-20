7953019aa841e91e7b704576990690589b7577a7
jimpescador <11jimuellep@gmail.com>
2024-01-20 11:54:07 +0800

    (Test) Welcome, Login & Register
    putting function to buttons direct to other Activity

--- ZERINE/app/src/main/java/Login.java
+++ ZERINE/app/src/main/java/Login.java
@@ -1,14 +1,26 @@
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.zerine.R;
 
-public class Welcome extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+        Button button1 = findViewById(R.id.LS_btnLogin);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Welcome.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- ZERINE/app/src/main/java/Register.java
+++ ZERINE/app/src/main/java/Register.java
@@ -1,14 +1,28 @@
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.zerine.R;
 
-public class Welcome extends AppCompatActivity {
-
+public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        Button button1 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Welcome.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
