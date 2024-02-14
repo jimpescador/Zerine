ac036f121aadbd01e55e091ae8b3623a299af01c
jimpescador <11jimuellep@gmail.com>
2024-02-14 19:40:09 +0800

    Added last and first name

--- ZERINE/app/src/main/java/com/example/zerine/Register.java
+++ ZERINE/app/src/main/java/com/example/zerine/Register.java
@@ -1,7 +1,6 @@
 package com.example.zerine;
 import static android.app.ProgressDialog.show;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,25 +10,17 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.zerine.R;
-import android.database.sqlite.SQLiteDatabase;
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import com.example.zerine.MyDBHelper;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Register extends AppCompatActivity {
-    EditText editTextUser, editTextPass, editTextEmail;
+    EditText editTextUser, editTextPass, editTextEmail, editTextFname, editTextLname;
     FirebaseFirestore firestore;
     ProgressBar progressBar;
 
@@ -41,8 +32,8 @@ public class Register extends AppCompatActivity {
         editTextUser = findViewById(R.id.reg_user);
         editTextPass = findViewById(R.id.reg_pass);
         editTextEmail = findViewById(R.id.reg_email);
-        editTextFname = findViewById(R.id.reg_email);
-        editTextLname = findViewById(R.id.reg_email);
+        editTextFname = findViewById(R.id.reg_fname);
+        editTextLname = findViewById(R.id.reg_lname;
         
         Button button1 = findViewById(R.id.btnRegisterAccount);
 
